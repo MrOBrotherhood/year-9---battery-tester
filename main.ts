2e59182351a99238aed0a5c463631e6cba11622a
MrOBrotherhood <113779645+MrOBrotherhood@users.noreply.github.com>
2022-09-17 14:16:59 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
+})
+input.onButtonPressed(Button.B, function () {
+    millaVolts = Math.round(pins.analogReadPin(AnalogPin.P0) * 3000 / 1023)
+    basic.showNumber(millaVolts)
+    if (millaVolts < 1200) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+let millaVolts = 0
+basic.showString("Battery Tester")
+basic.forever(function () {
+	
+})
